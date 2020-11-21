f234751df3303332ea7517998148a3878ff20c4c
Nuthi-Sriram <46497115+Nuthi-Sriram@users.noreply.github.com>
2020-11-21 09:47:06 +0530

    added userdatabase table creation

--- crud_db.sql
+++ crud_db.sql
@@ -196,3 +196,12 @@ ALTER TABLE Outing ADD constraint outing1 FOREIGN KEY(reg_no) REFERENCES Student
 ADD constraint outing2 FOREIGN KEY
 (staff_id) REFERENCES Staff
 (staff_id);
+
+CREATE TABLE `users` (
+ `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+ PRIMARY KEY (`id`),
+ UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
