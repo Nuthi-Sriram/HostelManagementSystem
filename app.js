decf57cb1d543bb7549696f449a53ea8e456635b
Nuthi-Sriram <46497115+Nuthi-Sriram@users.noreply.github.com>
2020-11-21 09:46:34 +0530

    mysql2 setup

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const hbs = require('hbs');
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
+const dbConnection = require('./database');
 dotenv.config();
 const app = express();
 //const dbService = require('./dbService');
 
 //const {getHomePage} = require('./routes/index');
 //const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
-const port = 5000
+const port = 3000
 app.use(cors());
-app.use(express.json());
-
+app.use(express.json());  
+ 
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 
@@ -25,7 +29,27 @@ const db = mysql.createConnection({
   password: 'Password123@',
   database: 'socka'
 });
+// APPLY COOKIE SESSION MIDDLEWARE
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  maxAge:  3600 * 1000 // 1hr
+}));
 
+// DECLARING CUSTOM MIDDLEWARE
+// const ifNotLoggedin = (req, res, next) => {
+//   if(!req.session.isLoggedIn){
+//       return res.render('login');
+//   }
+//   next();
+// }
+const ifLoggedin = (req,res,next) => {
+  if(req.session.isLoggedIn){
+      return res.redirect('/');
+  }
+  next();
+}
+// END OF CUSTOM MIDDLEWARE
 
 //connect to database
 db.connect((err) => {
@@ -213,6 +237,9 @@ app.get("/website-contact", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login");
 });
+app.get("/sign-up", (req, res) => {
+  res.render("sign-up");
+});
 app.get("/website-student-dashboard", (req, res) => {
   res.render("website-student-dashboard");
 });
@@ -227,8 +254,120 @@ app.get("/website-warden-dashboard", (req, res) => {
 // app.get('/delete/:id', deletePlayer);
 // app.post('/add', addPlayer);
 // app.post('/edit/:id', editPlayer);
+// REGISTER PAGE
+app.post('/register', ifLoggedin, 
+// post data validation(using express-validator)
+[
+    body('user_email','Invalid email address!').isEmail().custom((value) => {
+        return dbConnection.execute('SELECT `email` FROM `users` WHERE `email`=?', [value])
+        .then(([rows]) => {
+            if(rows.length > 0){
+                return Promise.reject('This E-mail already in use!');
+            }
+            return true;
+        });
+    }),
+    body('user_name','Username is Empty!').trim().not().isEmpty(),
+    body('user_pass','The password must be of minimum length 6 characters').trim().isLength({ min: 6 }),
+],// end of post data validation
+(req,res,next) => {
+
+    const validation_result = validationResult(req);
+    const {user_name, user_pass, user_email} = req.body;
+    // IF validation_result HAS NO ERROR
+    if(validation_result.isEmpty()){
+        // password encryption (using bcryptjs)
+        bcrypt.hash(user_pass, 12).then((hash_pass) => {
+            // INSERTING USER INTO DATABASE
+            dbConnection.execute("INSERT INTO `users`(`name`,`email`,`password`) VALUES(?,?,?)",[user_name,user_email, hash_pass])
+            .then(result => {
+                res.send(`your account has been created successfully, Now you can <a href="/">Login</a>`);
+            }).catch(err => {
+                // THROW INSERTING USER ERROR'S
+                if (err) throw err;
+            });
+        })
+        .catch(err => {
+            // THROW HASING ERROR'S
+            if (err) throw err;
+        })
+    }
+    else{
+        // COLLECT ALL THE VALIDATION ERRORS
+        let allErrors = validation_result.errors.map((error) => {
+            return error.msg;
+        });
+        // REDERING login-register PAGE WITH VALIDATION ERRORS
+        res.render('sign-up',{
+            register_error:allErrors,
+            old_data:req.body
+        });
+    }
+});// END OF REGISTER PAGE
+
+// LOGIN PAGE
+app.post('/login', ifLoggedin, [
+  body('user_email').custom((value) => {
+      return dbConnection.execute('SELECT `email` FROM `users` WHERE `email`=?', [value])
+      .then(([rows]) => {
+          if(rows.length == 1){
+              return true;
+              
+          }
+          return Promise.reject('Invalid Email Address!');
+          
+      });
+  }),
+  body('user_pass','Password is empty!').trim().not().isEmpty(),
+], (req, res) => {
+  const validation_result = validationResult(req);
+  const {user_pass, user_email} = req.body;
+  if(validation_result.isEmpty()){
+      
+      dbConnection.execute("SELECT * FROM `users` WHERE `email`=?",[user_email])
+      .then(([rows]) => {
+          // console.log(rows[0].password);
+          bcrypt.compare(user_pass, rows[0].password).then(compare_result => {
+              if(compare_result === true){
+                  req.session.isLoggedIn = true;
+                  req.session.userID = rows[0].id;
+
+                  res.redirect('/');
+              }
+              else{
+                  res.render('login-register',{
+                      login_errors:['Invalid Password!']
+                  });
+              }
+          })
+          .catch(err => {
+              if (err) throw err;
+          });
 
 
+      }).catch(err => {
+          if (err) throw err;
+      });
+  }
+  else{
+      let allErrors = validation_result.errors.map((error) => {
+          return error.msg;
+      });
+      // REDERING login-register PAGE WITH LOGIN VALIDATION ERRORS
+      res.render('login-register',{
+          login_errors:allErrors
+      });
+  }
+});
+// END OF LOGIN PAGE
+
+// LOGOUT
+app.get('/logout',(req,res)=>{
+  //session destroy
+  req.session = null;
+  res.redirect('/');
+});
+// END OF LOGOUT
 // set the app to listen on the port
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
